4300dcaebba98bd3721398c6c9f6a26b8eecd9d1
aenygma <uttieg@gmail.com>
2019-03-03 21:27:04 -0800

    wip. got most things working....ish

--- experiments/p5js_tests/circles.js
+++ experiments/p5js_tests/circles.js
@@ -16,81 +16,66 @@ define('circles', ['d3'], function (d3){
       }
     return draw;
 });
-
-mydefine('defaultPlotSketch', ['p5.dom', 'p5'], function(p) {
-console.log('test');
-console.log("p:", p);
-  function plot(container, data){
-    console.log("plot:",  container, data);
-	  // Initial setup
-	  p.setup = function() {
-	  	// Create the canvas
-	  	var canvas = p.createCanvas(500, 350);
-	  	p.background(150);
-
-	  	// Prepare the points for the plot
-	  	var points = [];
-	  	var seed = 100 * p.random();
-
-	  	for (var i = 0; i < 100; i++) {
-	  		points[i] = new GPoint(i, 10 * p.noise(0.1 * i + seed));
-	  	}
-
-	  	// Create a new plot and set its position on the screen
-	  	var plot = new GPlot(p);
-	  	plot.setPos(25, 25);
-
-	  	// Set the plot title and the axis labels
-	  	plot.setPoints(points);
-	  	plot.getXAxis().setAxisLabelText("x axis");
-	  	plot.getYAxis().setAxisLabelText("y axis");
-	  	plot.setTitleText("A very simple example");
-
-	  	// Draw it!
-	  	plot.defaultDraw();
-
-	  	p.noLoop();
-	  };
-  };
-  return plot;
-});
 */
 
-mydefine('circles', 
-  ['p5', 'p5.dom', 'grafica'], //'defaultPlotSketch'], 
-  function (p5, p5d, g){
-    console.log('ksjdlfjs');
+mydefine('simple_cos', 
+  ['p5', 'p5.dom', 'grafica', 'lodash'],
+  function (p5, p5d, g, _){
     function draw(container, data){
-      
       console.log('it works');
       console.log("pd:", p5d);
       console.log("g:", g);
       console.log(container);
       console.log(data);
+      console.log('lodash', _);
 
+      // Where to attach
       var sketch_div = document.createElement('div');
       sketch_div.setAttribute('id', 'cowabunga');
       sketch_div.style.position = "relative";
       container.appendChild(sketch_div);
 
-      //defaultPlotSketch(container);
-      //console.log('start p5');
+      var controls_div = document.createElement('div');
+      controls_div.setAttribute('id', 'ctrlz');
+      container.appendChild(controls_div);
+
       var sketch = new p5(
+        // @p is passed in by p5, when instantiated with a HTML Element
         function(p){
 
-          var rSlider;
+          var ampSlider;
+          var freqSlider;
+          var phaseSlider;
+
           var plot;
           var points;
 
+          var xValues = _.range(-2*Math.PI, Math.PI, 0.01);
+
           var createPoints = function(){
-            let rpoints = [];
+            let aVal = ampSlider.value();
+            let fVal = freqSlider.value();
+            let pVal = phaseSlider.value();
+
             // Prepare the points for the plot
-            var seed = 100 * p.random();
-            for (var i = 0; i < rSlider.value(); i+=1) {
-              //rpoints[i] = new GPoint(i, 10 * p.noise(0.1 * i + seed));
-              rpoints[i] = new GPoint(i, 10 * Math.sin(i/10.));
+            return _.map(xValues,
+              function(t){
+                return new GPoint(
+                  t, 
+                  aVal * Math.cos(2 * Math.PI * fVal * t + pVal))
+              });
+          }
+
+          function createMySlider(t){
+            /*
+              t
+            */
+            var s = p.createSlider(t.start, t.end, t.default, t.increment);
+            s.position(t.x, t.y);
+            for(var i in t.style){
+              s.style(i, t.style[i]);
             }
-            return rpoints;
+            return t;
           }
         
           // Initial setup
@@ -98,21 +83,53 @@ mydefine('circles',
             // Create the canvas
             var canvas = p.createCanvas(500, 350);
             p.background(150);
+
+            var inputPadding = 30;
         
-            // create slider
-            rSlider = p.createSlider(0, 255, 10);
-            rSlider.position(500, 0);
-            rSlider.style('width', '200px');
-            rSlider.style('position', 'relative');
-            rSlider.input(update);
+            // create sliders
+            //  amplitude
+            var ampSliderTxt = p.createElement('h3', "Amplitude:");
+            ampSliderTxt.position(500, 0);
+            ampSlider = p.createSlider(0, 5, 1, 0.1);
+            ampSlider.position(ampSliderTxt.x, 0);
+            ampSlider.style('width', '200px');
+            ampSlider.style('position', 'relative');
+            ampSlider.style('margin', '10px');
+            ampSlider.style('padding', '10px');
+            ampSlider.input(update);
+
+            // attach/render
+            ampSlider.parent(controls_div);
+            ampSliderTxt.parent(controls_div);
         
+            //  frequency
+            freqSlider = p.createSlider(1, 10, 1, 0.1);
+            freqSlider.position(500, inputPadding);
+            freqSlider.style('width', '200px');
+            freqSlider.style('position', 'relative');
+            freqSlider.style('margin', '10px');
+            freqSlider.style('padding', '10px');
+            freqSlider.input(update);
+
+            //  phase
+            phaseSlider = p.createSlider(0, 2*Math.PI, 0, 0.1);
+            phaseSlider.position(500, 2*inputPadding);
+            phaseSlider.style('width', '200px');
+            phaseSlider.style('position', 'relative');
+            phaseSlider.style('margin', '10px');
+            phaseSlider.style('padding', '10px');
+            phaseSlider.input(update);
+ 
             // Prepare the points for the plot
-            //var points = [];
-        
             points = createPoints();
+            console.log("points:", points);
         
             // Create a new plot and set its position on the screen
             plot = new GPlot(p);
+            plot.setXLim(-2, 2);
+            // TODO link these with ampSlider min/max
+            plot.setYLim(-5, 5);
+
             plot.setPointSize(0);
             plot.setPos(25, 25);
         
@@ -125,58 +142,17 @@ mydefine('circles',
             // Draw it!
             plot.defaultDraw();
         
-            //p.noLoop();
+            p.noLoop();
           };
         
           var update = function () {
         
             points = createPoints();
-            console.log("58", points);
-            // Create a new plot and set its position on the screen
-            //plot = new GPlot(p);
-        
-            // Set the plot title and the axis labels
+            console.log("points:", points);
             plot.setPoints(points);
-        
-            // Draw it!
             plot.defaultDraw();
-        
-        
           }
 
-/*          
-
-          // Initial setup
-          p.setup = function() {
-
-            // Create the canvas
-            var canvas = p.createCanvas(500, 350);
-            p.background(150);
-  
-            // Prepare the points for the plot
-            var points = [];
-            var seed = 100 * p.random();
-  
-            for (var i = 0; i < 100; i++) {
-              points[i] = new GPoint(in, 
-                            10 * p.noise(0.1 * i + seed));
-            }
-  
-            // Create a new plot and set its position on the screen
-            var plot = new GPlot(p);
-            plot.setPos(25, 25);
-  
-            // Set the plot title and the axis labels
-            plot.setPoints(points);
-            plot.getXAxis().setAxisLabelText("x axis");
-            plot.getYAxis().setAxisLabelText("y axis");
-            plot.setTitleText("A very simple example");
-  
-            // Draw it!
-            plot.defaultDraw();
-            p.noLoop();
-          };
-*/
         }, sketch_div);
     }
     return draw;
